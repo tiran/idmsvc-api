eacf950959e77ddbd0f578231a616c94c3aed474
Christian Heimes <cheimes@redhat.com>
2023-07-12 17:29:55 +0200

    fix: Address ruff code violation and simplify script
    There is no need to use `__main__` check here or catch the exception.
    Let the exception bubbled up and exit interpreter with full traceback
    and non-zero exit code.
    
    ```
    ruff run-test: commands[0] | ruff check .
    api/yaml2json.py:12:1: I001 [*] Import block is un-sorted or un-formatted
    api/yaml2json.py:14:8: F401 [*] `sys` imported but unused
    api/yaml2json.py:27:9: T201 `print` found
    api/yaml2json.py:28:9: PLR1722 [*] Use `sys.exit()` instead of `exit`
    Found 4 errors.
    [*] 3 potentially fixable with the --fix option.
    ```
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- yaml2json.py
+++ yaml2json.py
@@ -11,18 +11,12 @@ file and the output file.
 
 import argparse
 import json
-import sys
 import yaml
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert YAML to JSON")
-    parser.add_argument("infile", type=argparse.FileType(mode="r", encoding="utf-8"))
-    parser.add_argument("outfile", type=argparse.FileType(mode="w", encoding="utf-8"))
+parser = argparse.ArgumentParser(description="Convert YAML to JSON")
+parser.add_argument("infile", type=argparse.FileType(mode="r", encoding="utf-8"))
+parser.add_argument("outfile", type=argparse.FileType(mode="w", encoding="utf-8"))
 
-    try:
-        args = parser.parse_args()
-        data = yaml.safe_load(args.infile)
-        json.dump(data, args.outfile, indent="  ")
-    except Exception as ex:
-        print(ex)
-        exit(1)
+args = parser.parse_args()
+data = yaml.safe_load(args.infile)
+json.dump(data, args.outfile, indent="  ")
